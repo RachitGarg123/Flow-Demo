cc453171188fdea65693e35d25a48ac8b4202dc0
Rachit <rachit.garg@appinventiv.com>
2024-02-19 23:21:09 +0530

    Merge zip and combine tested

--- app/src/main/java/com/example/flowdemo/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/flowdemo/viewmodel/HomeViewModel.kt
@@ -9,6 +9,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.flowOn
@@ -16,6 +17,7 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.merge
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.withIndex
+import kotlinx.coroutines.flow.zip
 import kotlinx.coroutines.launch
 
 class HomeViewModel: ViewModel() {
@@ -24,7 +26,7 @@ class HomeViewModel: ViewModel() {
 
     fun getLatestInteger() {
         viewModelScope.launch {
-            val flowOne = repo.latestInteger
+            val integerFlow = repo.latestInteger
                 .filter { num ->  (num % 2) == 0 } //Intermediate operators used in flow to manipulate data
                 .map { filteredNum -> filteredNum + 9 }
 //                .withIndex() // map values with indexes
@@ -38,7 +40,7 @@ class HomeViewModel: ViewModel() {
 ////                    delay(500L) // If Items are producing faster than consumed and we are using collectLatest then only latest value will be collected unlike collect which collects all values
 //                    Log.i("Integer","latest integer ---> $latestInteger")
 //                }
-            val flowTwo = repo.latestString
+            repo.latestString
                 .filter { num ->  (num.toInt() % 2) == 0 } //Intermediate operators used in flow to manipulate data
                 .map { filteredNum -> filteredNum + 9 }
 //                .withIndex() // map values with indexes
@@ -48,29 +50,34 @@ class HomeViewModel: ViewModel() {
                     emit("7777") // we can catch exceptions and also emit values
 //                    emit(IndexedValue(777, 7777)) // when using withIndex Operator
                 }
-//                .collectLatest{ latestInteger -> // terminal operator to fetch data from flow
-////                    delay(500L) // If Items are producing faster than consumed and we are using collectLatest then only latest value will be collected unlike collect which collects all values
-//                    Log.i("Integer","latest integer ---> $latestInteger")
+//                .combine(integerFlow) { valueOne, valueTwo -> // In case of combine operator it will not wait for the other flow to make pair with it instead it will let the values come and make pair ith the value currently flowing and als it makes flow with every value from other flow
+//                    "valueOne ---> $valueOne valueTwo -----> $valueTwo"
 //                }
-            val finalResult = merge(flowOne, flowTwo)
-            finalResult.collectLatest {
-                Log.i("MergedValues", "after merging ----> $it") // output ----> merged values coming from both flows in any unpredictable order
-            }
+//                .zip(integerFlow) { valueOne, valueTwo -> // In case of zip operator values from both flows are zipped {collected from the output of zip including change in data type} and will wait for the other flow to emit a value to form a pair and minimum size will be taken i.e if one flow emits 1 value and other emits 10 values then only 1 pair will be formed
+//                    "valueOne ---> $valueOne valueTwo -----> $valueTwo"
+//                }
+                .collectLatest{ latestInteger -> // terminal operator to fetch data from flow
+//                    delay(500L) // If Items are producing faster than consumed and we are using collectLatest then only latest value will be collected unlike collect which collects all values
+                    Log.i("Integer","latest integer ---> $latestInteger")
+                }
+//            val finalResult = merge(flowOne, flowTwo)
+//            finalResult.collectLatest {
+//                Log.i("MergedValues", "after merging ----> $it") // output ----> merged values coming from both flows in any unpredictable order
+//            }
         }
     }
 
-    fun zipAndCombine() {
-        viewModelScope.launch {
-            val numbers = (1..3).asFlow().onEach {
-                delay(300)
-            }
-            val numberOfTexts = flowOf("one", "two", "three").onEach {
-                delay(400)
-            }
-            val startTime = System.currentTimeMillis()
+/*    one example of merge zip and combine operators
 
-        }
-    }
+    flow1 -> {1, 2, 3, 4, 5}
+    flow2 -> {6, 7, 8}
+
+    merge -> {1, 6, 2, 3, 7, 8, 4, 5} -> merged both flows in any order
+
+    let's say we add delay of 1000 ms in flowOne and 400 ms in flowTwo then
+    flow.zip -> {{1, 6}, {2, 7}, {3, 8}} // we can manipulate values according to our will
+
+    flow.combine -> {{1, 8}, {2, 8}, {3, 8}, {4, 8}, {5, 8}} // since first flow is having delay of 1 sec then current value from flow2 will be 8 then such pair is formed in combine*/
 }
 
 
